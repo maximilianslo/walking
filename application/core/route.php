b010540d26a8cf96cc18ef3c1944a18029c92f78
Slovenberg <islovenberg@mail.ru>
2020-03-04 11:34:40 +0300

    Stash

--- application/core/route.php
+++ application/core/route.php
@@ -21,8 +21,8 @@ class Route
             $action_name = $routes[2];
         }
 
-        $model_name = 'Model_' . $controller_name;
-        $controller_name = 'Controller_' . $controller_name;
+        $model_name = 'Model' . $controller_name;
+        $controller_name = 'Controller' . $controller_name;
         $action_name = 'action_' . $action_name;
 
 
